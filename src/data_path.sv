9892b393c587821f5c9eede31c8db28d1fdd0467
Julia Souza <julia-souza@uergs.edu.br>
2022-11-12 21:25:11 -0300

    adicionando parte 2

--- src/data_path.sv
+++ src/data_path.sv
@@ -19,28 +19,54 @@ import k_and_s_pkg::*;
     output logic              [4:0] ram_addr,
     output logic             [15:0] data_out,
     input  logic             [15:0] data_in,
-    
-    input logic [5:0] A,
-    input logic [5:0] B
+   
+    input logic [4:0] B
 );
 
 
-logic [5:0] pc_reg;
-logic soma;
+logic [4:0] pc_reg;
+logic [15:0] ir_reg;
+logic sum;
 logic branch_out;
 logic addr_out;
+logic [4:0] a_addr;
+logic [4:0] b_addr;
+logic [4:0] c_addr;
+logic [4:0] mem_addr;
 
-assign soma = pc_reg + B;
+// Decode - Verificar como fazer
+always_comb begin
+  case(ir_reg)
+    3'b100: a_addr = 4'b1110;
+    3'b101: b_addr = 4'b1110;
+    3'b110: c_addr = 4'b1110;
+    3'b111: mem_addr = 4'b1110;
+    default: decoded_instruction[0] = 4'b0000;
+  endcase
+end
 
-assign branch_out = (branch?A:soma);
+// Soma com PC_REG com B - Verificar o que é B
+assign sum = pc_reg + B;
+
+assign branch_out = (branch?mem_addr:sum);
 
 always_ff @(posedge clk) begin
   if (pc_enable)
     pc_reg <= branch_out;
 end
 
-assign addr_out = (addr_sel?A:pc_reg);
+assign addr_out = (addr_sel?mem_addr:pc_reg);
 
 assign ram_addr = addr_out;
+
+always_ff @(posedge clk) begin
+  if (ir_enable)
+    ir_reg <= data_in;
+end
+
+// Banco de registradores - Verificar default para 2 bits
+logic [4:0] rf [4] = '{ default: 8'd87};
+
+    // Paramos aqui
 
 endmodule : data_path
